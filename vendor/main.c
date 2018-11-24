55b030edaf0558656f682131e994c33f7483a3ae
Marcos Minond <minond.marcos@gmail.com>
2018-11-23 21:55:24 -0700

    encrypt_encode + decode_decrypt

--- vendor/main.c
+++ vendor/main.c
@@ -3,63 +3,125 @@
 #include "libhydrogen/hydrogen.c"
 #include "stringencoders/src/modp_b64.c"
 
-size_t encrypt_encode(char *dest, const void *message, uint64_t message_id,
+/**
+ * Encrypts and encodes a message using hydro_secretbox_encrypt and
+ * modp_b64_encode functions. Size of the dest parameter should be:
+ *
+ *     modp_b64_encode_len(size of message + hydro_secretbox_HEADERBYTES);
+ *
+ *
+ * @example
+ *
+ *     const char* message = "My secret message";
+ *     const char* context = "examples";
+ *     const uint8_t key[hydro_secretbox_KEYBYTES];
+ *     hydro_secretbox_keygen(key);
+ *
+ *     int max = modp_b64_encode_len(strlen(message) + hydro_secretbox_HEADERBYTES);
+ *     char* dest[max];
+ *     size_t size = encrypt_encode(dest, message, strlen(message), 0, context, key);
+ *
+ *
+ * @param dest stores the output string
+ * @param message is the original message to encrypt and encode
+ * @param message_len is the size of the message parameter
+ * @param message_id XXX
+ * @param context XXX
+ * @param key XXX
+ * @return size of the encoded string
+ */
+size_t encrypt_encode(char* dest, const char* message, size_t message_len,
+                      uint64_t message_id,
                       const char context[hydro_secretbox_CONTEXTBYTES],
                       const uint8_t key[hydro_secretbox_KEYBYTES]) {
-  int enc_len = hydro_secretbox_HEADERBYTES + strlen(message);
-  uint8_t ciphertext[enc_len];
-  int status = hydro_secretbox_encrypt(ciphertext, message, strlen(message),
-                                       message_id, context, key);
+  int cipher_len = message_len + hydro_secretbox_HEADERBYTES;
+  uint8_t cipher[cipher_len];
+  int status = hydro_secretbox_encrypt(cipher, message, message_len, message_id,
+                                       context, key);
 
-  // Something wen't wrong with the encryption. Skip encoding and return 0 as
-  // the size of the encoding string.
   if (status != 0) {
     return 0;
   }
 
-  return modp_b64_encode(dest, ciphertext, enc_len);
+  return modp_b64_encode(dest, cipher, cipher_len);
 }
 
-int main(int argc, char **argv) {
+/**
+ * Decodes and decrypts a message using modp_b64_decode and
+ * hydro_secretbox_decrypt functions. Since the estimated decoded size is not
+ * always accurate, it is safest to allocate a little more than required and
+ * then clean up the result using the real size, which is returned by this
+ * function:
+ *
+ *     int max = modp_b64_decode_len(size of message);
+ *     size_t real = decode_decrypt(...);
+ *
+ * @example
+ *
+ *     // Assuming a key and context already exist, as well as a message, which
+ *     // is the encoded and encoded message.
+ *     int max = modp_b64_decode_len(strlen(message));
+ *     char* dest[max];
+ *     size_t size = decode_decrypt(dest, message, strlen(message), 0, context, key);
+ *
+ *
+ * @param dest stores the output string
+ * @param message is the message to decode and decrypt
+ * @param message_len is the size of the message parameter
+ * @param message_id XXX
+ * @param context XXX
+ * @param key XXX
+ * @return size of the decrypted string
+ */
+size_t decode_decrypt(char* dest, const void* message, size_t message_len,
+                      uint64_t message_id,
+                      const char context[hydro_secretbox_CONTEXTBYTES],
+                      const uint8_t key[hydro_secretbox_KEYBYTES]) {
+  int max_decoded_len = modp_b64_decode_len(message_len);
+  char* decoded[max_decoded_len];
+  int decoded_len = modp_b64_decode(decoded, message, message_len);
+  int status = hydro_secretbox_decrypt(dest, decoded, decoded_len, message_id,
+                                       context, key);
+
+  if (status != 0) {
+    return 0;
+  }
+
+  return decoded_len - hydro_secretbox_HEADERBYTES;
+}
+
+int main(int argc, char** argv) {
   (void)argc;
   (void)argv;
 
-  if (hydro_init() != 0) {
-    abort();
-  }
+  const char* message =
+      "The C Programming Language (sometimes termed K&R, after its authors' "
+      "initials) is a computer programming book written by Brian Kernighan and "
+      "Dennis Ritchie, the latter of whom originally designed and implemented "
+      "the language, as well as co-designed the Unix operating system with "
+      "which development of the language was closely intertwined. The book was "
+      "central to the development and popularization of the C programming "
+      "language and is still widely read and used today. Because the book was "
+      "co-authored by the original language designer, and because the first "
+      "edition of the book served for many years as the de facto standard for "
+      "the language, the book was regarded by many to be the authoritative "
+      "reference on C.";
 
-  char *context = "examples";
-  char *text = "testing 3 2 1!";
-
-  size_t msgsize = hydro_secretbox_HEADERBYTES + strlen(text);
-
-  uint8_t key[hydro_secretbox_KEYBYTES];
-  uint8_t ciphertext[hydro_secretbox_HEADERBYTES + strlen(text)];
-
+  const char* context = "examples";
+  const uint8_t key[hydro_secretbox_KEYBYTES];
   hydro_secretbox_keygen(key);
-  hydro_secretbox_encrypt(ciphertext, text, strlen(text), 0, context, key);
 
-  char decrypted[strlen(ciphertext) - hydro_secretbox_HEADERBYTES];
-  if (hydro_secretbox_decrypt(decrypted, ciphertext, strlen(ciphertext), 0,
-                              context, key) != 0) {
-    printf("error");
-  }
+  int encrypt_encode_len =
+      modp_b64_encode_len(strlen(message) + hydro_secretbox_HEADERBYTES);
+  char* encoded_ptr[encrypt_encode_len];
+  size_t size =
+      encrypt_encode(encoded_ptr, message, strlen(message), 0, context, key);
 
-  printf("decrypted: %s\n", decrypted);
-
-  char *str = "testing 1 2 3!";
-  printf("encoded size is %d\n", modp_b64_encode_len(strlen(str)));
-  char *encoded[modp_b64_encode_len(strlen(str))];
-  memset(encoded, 0, modp_b64_encode_len(strlen(str)));
-
-  modp_b64_encode(encoded, str, strlen(str));
-  printf("%s\n", encoded);
-
-  char *decoded[modp_b64_decode_len(strlen(encoded))];
-  memset(decoded, 0, modp_b64_encode_len(strlen(str)));
-
-  size_t s = modp_b64_decode(decoded, encoded, strlen(encoded));
-  printf("%zu, %s\n", s, decoded);
-
-  return 0;
+  int max_size = modp_b64_decode_len(strlen(encoded_ptr));
+  char* result[max_size];
+  int real_size =
+      decode_decrypt(result, encoded_ptr, strlen(encoded_ptr), 0, context, key);
+  printf("max_size = %d\n", max_size);
+  printf("size = %d\n", real_size);
+  printf("result = %s\n", result);
 }
